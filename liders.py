a3d6d97b02cf1bc10fb832dbd296fd648599c474
Luiz Carlos Lourenþo <1900963@SALAS.AULAS>
2019-10-28 11:28:39 -0300

    Lista de cursos e Detalhe de Cursos

--- liders.py
+++ liders.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-            return render_template("index.html")
+@app.route('/listadecursos')
+def listadecursos():
+            return render_template("Lista de Cursos.html")
 
-@app.route('/disciplina')
-def disciplina():
-            return render_template("Disciplina.html") 
-           
-@app.route('/noticias')
-def noticias():
-            return render_template("Noticias.html")
+
+@app.route('/detalhedecurso')
+def detalhedecurso():
+            return render_template("Detalhe de Curso.html")
+
 app.run()
