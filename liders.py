c65fdfc0f49f94b4ccf2b9bc68d93097031529bc
ItalodeSouza <53847195+ItalodeSouza@users.noreply.github.com>
2019-10-28 11:40:06 -0300

    Update liders.py

--- liders.py
+++ liders.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask (__name__)
 
-@app.route('/listadecursos')
-def listadecursos():
-            return render_template("Lista de Cursos.html")
+@app.route( '/listadecursos' )
+def listadecursos ( ):
+            return render_template("Lista de Cursos.html" )
 
 
-@app.route('/detalhedecurso')
-def detalhedecurso():
-            return render_template("Detalhe de Curso.html")
+@app.route('/detalhedecurso' )
+def detalhedecurso ():
+            return render_template( "Detalhe de Curso.html")
 
-app.run()
+app.run( )
